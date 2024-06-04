13d5b54857f67d018b0ccf67591bba7cfce3bf3e
rdias66 <dias.a@live.com>
2024-06-04 13:30:46 -0300

    events model update

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
-
+  validates :category, presence: true, uniqueness: false
+  validates :content, presence: true, uniqueness: false
   # Soft delete
   scope :active, -> { where(deleted_at: nil) }
 end
