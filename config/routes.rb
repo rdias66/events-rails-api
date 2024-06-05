eec5b58d440e64640095eced3ba4335405803fed
rdias66 <dias.a@live.com>
2024-06-05 11:43:04 -0300

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
- 
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :users, except: [:new, :edit] do
+    member do
+      delete 'soft_delete', to: 'users#destroy'
+    end
+  end
 
+  resources :events, except: [:new, :edit] do
+    collection do
+      get 'user/:user_id', to: 'events#user_events', as: 'user_events'
+    end
+    member do
+      delete 'soft_delete', to: 'events#destroy'
+    end
+  end
 end
-
-Rails.application.routes.draw do
-  resources :users
-  resources :events
-end
-
